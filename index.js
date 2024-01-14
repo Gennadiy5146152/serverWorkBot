4d1e3db7c61b0a7b1f24a7d37b41eb0315dc7ae9
Gennadiy <ideamen51@gmail.com>
2024-01-14 18:01:06 +0500

    сделал отправку сообщения в отдельную тему, сделал команду для запроса статуса

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 const fetch = require("node-fetch");
-const TelegramBot = require("node-telegram-bot-api");
+const { Telegraf } = require("telegraf");
 const serverConfig = {
-  url: "https://korotkovs.com/",
+  url: "https://178.208.90.56",
   timeout: 5000,
 };
 
 const chatId = "-1002078171202";
+const message_thread_id = 3;
 
-const bot = new TelegramBot("6848774632:AAE0ldngGKgTjdriuEdqcCpTlxDhNCO03iM", {
-  polling: true,
+const bot = new Telegraf("6848774632:AAE0ldngGKgTjdriuEdqcCpTlxDhNCO03iM", {
+  handlerTimeout: 9_000_000,
 });
 async function checkServerStatus() {
   try {
-    const response = await fetch(serverConfig.url, {
-      timeout: serverConfig.timeout,
-    });
-    if (response.status !== 200) {
+    const response = await fetch(serverConfig.url);
+    if (!response.ok) {
+      bot.telegram.sendMessage(chatId, `Код ошибки: ${response.status}`, {
+        reply_to_message_id: 6172,
+      });
       throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
     }
   } catch (error) {
     console.error("Сервер недоступен:", error.message);
-    bot.sendMessage(chatId, "Сервер не работает");
+    bot.telegram.sendMessage(chatId, `Ошибка ${error.message}`, {
+      reply_to_message_id: 6172,
+    });
+    getStatusServer();
   }
 }
 
-setInterval(checkServerStatus, 60000);
+async function getStatusServer() {
+  try {
+    const response = await fetch(serverConfig.url);
+    bot.telegram.sendMessage(chatId, `Статус: ${response.status}`, {
+      reply_to_message_id: 6172,
+    });
+  } catch (error) {
+    bot.telegram.sendMessage(chatId, "Сервер не работает", {
+      reply_to_message_id: 6172,
+    });
+  }
+}
+
+bot.on("text", (ctx) => {
+  try {
+    bot.telegram.setMyCommands([
+      { command: "/server", description: "Узнать статус сервера" },
+    ]);
+    if (
+      ctx.message.text === "/server" ||
+      ctx.message.text === "/server@serverWorkKorotkovs_bot"
+    ) {
+      getStatusServer(ctx);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+setInterval(checkServerStatus, 5000);
+
+bot.launch();
